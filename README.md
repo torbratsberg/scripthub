f67e2f7e1e61d826c31b620ff345d760487e36b7
Tor Bratsberg <tor@bratsberg.net>
2021-10-21 18:24:45 +0200

    Chore, doc: Cleanup, refactor, documentation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A CLI tool to manage all your scripts scattered around on your computer.
 
 ## Usage
-- Prefix all commands with `scripthub` (Recommended: Set up `sch` as alias for `scripthub`)
+Prefix all commands with `scripthub` (Recommended: Set up `sch` as alias for `scripthub`)
 
 ### Commands
 - `list, ls` - List all available scripts
@@ -12,6 +12,8 @@ A CLI tool to manage all your scripts scattered around on your computer.
 	- `--name, -n` - Name of script to add
 	- `--executable, -x` - Relative path to executable script
 	- `--editable, -e` - Relative path to editable script (Optional. Defaults to `--executable`)
+- `remove, rm` - Remove the given script from your library
+	- `{name of script to remove}`
 - `run, r` - Run the given script
 	- `{name of script to edit}`
 - `path, p` - Get paths for given script
@@ -26,3 +28,18 @@ A CLI tool to manage all your scripts scattered around on your computer.
 ### Configuration
 - Change editor to open files in when running `edit`.
 	- Add this line to your shell config file: `export EDITOR="{YOUR FAVORITE EDITOR}"`
+
+## Installation
+1. Clone this repo
+2. Make sure you have Go installed
+3. `$ go install`
+3. `$ scripthub --help`
+
+## Development
+
+### Commands to add
+- `nuke` - Deletes both the editable and executable file, and removes script entry from scripthub
+
+### Known bugs
+- Can't delete last entry in scripts file
+
--- main.go
+++ main.go
@@ -18,6 +18,13 @@ type script struct {
 	editable   string
 }
 
+/*
+	==========
+	Utillities
+	==========
+*/
+
+// Returns the users home directory
 func getHomeDir() string {
 	homeDir, err := os.UserHomeDir()
 	check(err)
@@ -60,164 +67,185 @@ func getScripts() []script {
 	return scriptsStruct
 }
 
+// Returns the script struct of a given script
+func getScriptStruct(name string) (res script, err error) {
+	scripts := getScripts()
+
+	// Find the specified script
+	for i := 0; i < len(scripts); i += 1 {
+		if scripts[i].name == name {
+			res = scripts[i]
+			break
+		}
+	}
+
+	if res.name == "" {
+		err = fmt.Errorf("Error: Could not find script \"%s\" in scripts", name)
+	}
+	return
+}
+
+/*
+	================
+	Action functions
+	================
+*/
+
 // Appends a script to the script file
-func addScript(newScript script) error {
+func addScript(newScript script) (err error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
 	// Make the executable path
-	executablePath, err := os.Getwd()
-	newScript.executable = filepath.Join(executablePath, newScript.executable)
-	check(err)
+	newScript.executable = filepath.Join(cwd, newScript.executable)
 
 	// Check if editable is set, else use executable
 	if newScript.editable == "" {
 		newScript.editable = newScript.executable
 	} else {
-		editablePath, err := os.Getwd()
-		newScript.editable = filepath.Join(editablePath, newScript.editable)
-		check(err)
+		newScript.editable = filepath.Join(cwd, newScript.editable)
 	}
 
 	// Write new script entry to the scripts file
 	file, err := os.OpenFile(configScriptsFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
 	if err != nil {
-		log.Println(err)
-		return err
+		return
 	}
 
 	defer file.Close()
 
-	// Put togheter the script entry string
+	// Put togheter the script entry string and write to file
 	scriptString := newScript.name + " : " + newScript.executable + " : " + newScript.editable + "\n"
-
-	if _, err := file.WriteString(scriptString); err != nil {
-		log.Println(err)
-		return err
+	if _, err = file.WriteString(scriptString); err != nil {
+		return
 	}
-
 	file.Close()
 
-	return nil
+	return
 }
 
 // Opens the editable script file in the EDITOR env variable
-func editScript(editScript string) error {
+func editScript(name string) (err error) {
 	// Get all the scripts
 	scripts := getScripts()
 
+	var scriptPath string
+
 	// Find the editable path for specified script
 	for i := 0; i < len(scripts); i += 1 {
-		if scripts[i].name == editScript {
-			editScript = scripts[i].editable
+		if scripts[i].name == name {
+			scriptPath = scripts[i].editable
 			break
 		}
 	}
 
-	// Open script in neovim
-	cmd := exec.Command(os.Getenv("EDITOR"), editScript)
+	if scriptPath == "" {
+		return fmt.Errorf("Error: Could not find script \"%s\" in scripts", name)
+	}
+
+	// Open script in $EDITOR env variable (Defaults to `vim`)
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+
+	cmd := exec.Command(editor, scriptPath)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
-	err := cmd.Run()
+	err = cmd.Run()
 	check(err)
 
-	return nil
+	return
 }
 
 // Removes script from the script file
-func removeScript(name string) error {
-	scriptPaths, err := os.ReadFile(configScriptsFilePath)
-	check(err)
+func removeScript(name string) (err error) {
+	scriptsFile, err := os.ReadFile(configScriptsFilePath)
 
-	scriptLines := string(scriptPaths)
+	scriptLines := string(scriptsFile)
 
 	// Remove line starting with {name}
 	re := regexp.MustCompile("(?m)[\r\n]+^" + name + ".*$")
 	res := re.ReplaceAllString(scriptLines, "")
 
-	os.WriteFile(configScriptsFilePath, []byte(res), 0644)
+	// Overwrite the scripts file
+	os.WriteFile(configScriptsFilePath, []byte(res), 0777)
 
-	return nil
+	return
 }
 
 // Runs the script with the same name as passed in as argument
-func runScript(name string) error {
-	// Get all the scripts
-	scripts := getScripts()
-
+func runScript(name string) (err error) {
 	var executablePath string
 
-	// Find the editable path for specified script
-	for i := 0; i < len(scripts); i += 1 {
-		if scripts[i].name == name {
-			executablePath = scripts[i].executable
-		}
-	}
+	res, err := getScriptStruct(name)
+	check(err)
 
+	executablePath = res.executable
 	if executablePath == "" {
-		return fmt.Errorf("Error: Could not find script \"%s\" in scripts", name)
+		err = fmt.Errorf("Error: Could not find script \"%s\" in scripts", name)
 	}
 
 	cmd := exec.Command(executablePath)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
-	err := cmd.Run()
+	err = cmd.Run()
 	check(err)
 
-	return nil
+	return
 }
 
+// Returns the path of the given script, can be specified using the option param
 func getPath(name string, option string) (res string, err error) {
-	// Get all the scripts
-	scripts := getScripts()
+	temp, err := getScriptStruct(name)
+	check(err)
 
-	var temp script
-
-	// Find the editable path for specified script
-	for i := 0; i < len(scripts); i += 1 {
-		if scripts[i].name == name {
-			temp = scripts[i]
-			break
-		}
-	}
-
-	if temp.editable == "" || temp.executable == "" {
-		err = fmt.Errorf("Error: Could not find script \"%s\" in scripts", name)
+	if option == "executable" || option == "x" {
+		res = temp.executable
+	} else if option == "editable" || option == "e" {
+		res = temp.editable
 	} else {
-		if option == "executable" || option == "x" {
-			res = temp.executable
-		} else if option == "editable" || option == "e" {
-			res = temp.editable
-		} else {
-			res += fmt.Sprintf("Name       : %s\n", temp.name)
-			res += fmt.Sprintf("Editable   : %s\n", temp.editable)
-			res += fmt.Sprintf("Executable : %s\n", temp.executable)
-		}
+		res += fmt.Sprintf("Name       : %s\n", temp.name)
+		res += fmt.Sprintf("Editable   : %s\n", temp.editable)
+		res += fmt.Sprintf("Executable : %s\n", temp.executable)
 	}
 
 	return
 }
 
+// Sets up scripthub files and folders is needed
 func setup() {
+	// If scripts file does not exist is scripthub config folder
 	if _, err := os.Stat(configScriptsFilePath); os.IsNotExist(err) {
 		fmt.Println("Could not find scripts file. Generating one.")
 		fmt.Println("")
 
 		if _, err := os.Stat(configScripthubPath); os.IsNotExist(err) {
+			// If scripthub config folder does not exist
 			err := os.Mkdir(configScripthubPath, 0666)
 			check(err)
 			err = os.WriteFile(configScriptsFilePath, []byte(""), 0777)
 			check(err)
-			fmt.Println("Generated scripts file at: " + configScriptsFilePath)
+			fmt.Printf("Generated scripts file at: %s\n", configScriptsFilePath)
+		} else {
+			// If scripthub config folder does exist, but scripts file doesn't
+			err = os.WriteFile(configScriptsFilePath, []byte(""), 0777)
+			check(err)
+			fmt.Printf("Generated scripts file at: %s\n", configScriptsFilePath)
 		}
 	} else {
+		// Scriphub is already set up
 		fmt.Println("Scripts file found. No setup needed")
 		fmt.Println("Here are the scripts in your library: ")
 
 		scriptStructs := getScripts()
 		fmt.Println("")
 		for i := 0; i < len(scriptStructs); i += 1 {
-			fmt.Println("Name       : " + scriptStructs[i].name)
-			fmt.Println("Executable : " + scriptStructs[i].executable)
-			fmt.Println("Editable   : " + scriptStructs[i].editable)
+			fmt.Printf("Name       : %s\n", scriptStructs[i].name)
+			fmt.Printf("Executable : %s\n", scriptStructs[i].executable)
+			fmt.Printf("Editable   : %s\n", scriptStructs[i].editable)
 			fmt.Println("============")
 		}
 	}
@@ -237,7 +265,7 @@ func main() {
 				Name:    "list",
 				Aliases: []string{"ls"},
 				Usage:   "List all available scripts",
-				Action: func(*cli.Context) error {
+				Action: func(*cli.Context) (err error) {
 					scriptStructs := getScripts()
 
 					for i := 0; i < len(scriptStructs); i += 1 {
@@ -247,7 +275,7 @@ func main() {
 						fmt.Println("============")
 					}
 
-					return nil
+					return
 				},
 			},
 			// ================================================================
@@ -255,11 +283,11 @@ func main() {
 				Name:    "edit",
 				Aliases: []string{"e"},
 				Usage:   "Edit the given script",
-				Action: func(c *cli.Context) error {
+				Action: func(c *cli.Context) (err error) {
 					editScriptName := c.Args().First()
-					err := editScript(editScriptName)
+					err = editScript(editScriptName)
 					check(err)
-					return nil
+					return
 				},
 				ArgsUsage: "scriptName",
 			},
@@ -294,10 +322,10 @@ func main() {
 						Destination: &newScriptStruct.executable,
 					},
 				},
-				Action: func(c *cli.Context) error {
-					err := addScript(newScriptStruct)
+				Action: func(c *cli.Context) (err error) {
+					err = addScript(newScriptStruct)
 					check(err)
-					return nil
+					return
 				},
 			},
 			// ================================================================
@@ -306,11 +334,11 @@ func main() {
 				Aliases:   []string{"rm"},
 				Usage:     "Remove a script from your library",
 				ArgsUsage: "{script name}",
-				Action: func(c *cli.Context) error {
+				Action: func(c *cli.Context) (err error) {
 					name := c.Args().First()
-					err := removeScript(name)
+					err = removeScript(name)
 					check(err)
-					return nil
+					return
 				},
 			},
 			// ================================================================
@@ -319,11 +347,11 @@ func main() {
 				Aliases:   []string{"r"},
 				Usage:     "Run a script from your library",
 				ArgsUsage: "{script name}",
-				Action: func(c *cli.Context) error {
+				Action: func(c *cli.Context) (err error) {
 					name := c.Args().First()
-					err := runScript(name)
+					err = runScript(name)
 					check(err)
-					return nil
+					return
 				},
 			},
 			// ================================================================
@@ -336,25 +364,24 @@ func main() {
 					&cli.StringFlag{
 						Name:        "specifier",
 						Aliases:     []string{"s"},
-						Usage:       "Pass in `x` to only get executable path, or `e` to only get editable path",
 						Destination: &specifier,
 					},
 				},
-				Action: func(c *cli.Context) error {
+				Action: func(c *cli.Context) (err error) {
 					name := c.Args().First()
 					res, err := getPath(name, specifier)
 					fmt.Println(res)
 					check(err)
-					return nil
+					return
 				},
 			},
 			// ================================================================
 			{
 				Name:  "setup",
 				Usage: "Set up scripthub",
-				Action: func(c *cli.Context) error {
+				Action: func(c *cli.Context) (err error) {
 					setup()
-					return nil
+					return
 				},
 			},
 			// ================================================================
